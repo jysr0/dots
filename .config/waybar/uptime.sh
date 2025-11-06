349d8cf3266b749d2339f3a55d73def855a809cb
ysr <141933879+jysr0@users.noreply.github.com>
2025-11-06 23:50:36 +0100

    Add files via upload

--- .config/waybar/uptime.sh
+++ .config/waybar/uptime.sh
@@ -3,9 +3,9 @@
 while [ true ];
 do
 	if [[ $(uptime | awk '{print $3}') != *\:* ]]; then
-		echo "🛸 "$(uptime | awk '{print $3}')"Min"
+		echo "󰅕 "$(uptime | awk '{print $3}')"Min"
 	else
-		echo "🛸 $(uptime | awk '{print $3}' | tr -d ',' | awk -F':' '{print $1"H:", $2"Min"}' | tr -d ' ')"
+		echo "󰅕 $(uptime | awk '{print $3}' | tr -d ',' | awk -F':' '{print $1"H:", $2"Min"}' | tr -d ' ')"
 	fi
 	sleep 600
 done
