6b6e8ee77e881853e91cf6fc0655dc3147c06fbb
ysr <141933879+jysr0@users.noreply.github.com>
2025-11-18 16:34:59 +0100

    Delete .local/bin/dwm scripts/dwmb_uptime.sh

--- .local/bin/dwm scripts/dwmb_uptime.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# 🛸
-
-if [[ $(uptime | awk '{print $3}') != *\:* ]]; then
-	echo "󰅕 $(uptime | awk '{print $3}')Min"
-else
-	echo "󰅕 $(uptime | awk '{print $3}' | tr -d ',' | awk -F':' '{print $1"H:", $2"Min"}' | tr -d ' ')"
-fi
