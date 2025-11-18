02504f4a5b7e3c1fed1819b59c10eade065f9f9a
ysr <141933879+jysr0@users.noreply.github.com>
2025-11-18 16:31:50 +0100

    Delete .local/bin/dwm scripts/dmenu_bg.sh

--- .local/bin/dwm scripts/dmenu_bg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-dir="backgrounds"
-[[ -d $XDG_PICTURES_DIR/$dir ]] || (notify-send "'$dir' directory doesn't exist" && exit 1)
-[[ -d $XDG_CACHE_HOME ]] || (notify-send "create home 'cache' directory and try again" && exit 1)
-	bg="$(ls -A $XDG_PICTURES_DIR/$dir | dmenu -i -l 10 -p '󰸉 set background:')"
-	xwallpaper --stretch $XDG_PICTURES_DIR/$dir/$bg 2> /dev/null && cp $XDG_PICTURES_DIR/$dir/$bg $XDG_CACHE_HOME/bg.jpg
